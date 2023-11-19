6682aa69f368fe82e5390b068ed28e8ccff06073
jonas-sebera <sebejaz99@gmail.com>
2023-11-19 14:59:45 +0200

    init update

--- 1_accounts.js
+++ 1_accounts.js
@@ -1,13 +1,22 @@
-const { ethers }  = require("ethers");
-const INFURA_ID = [process.env.API_KEY_SECRET];
+const { ethers } = require("ethers");
+const INFURA_ID = process.env.API_KEY_SECRET;
 console.log(INFURA_ID);
 
-// use Infura node 
+// use Infura node
 
+const provider = new ethers.providers.JsonRpcProvider(
+  `https://mainnet.infura.io/${INFURA_ID}`
+);
 
- const provider = new ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/${INFURA_ID}`);
+const address = "0x1F334285EfdbE58034d3F24DD0703672E0741f00";
 
- 
-// get balance for address
-const balance = await provider.getBalance("0x1F334285EfdbE58034d3F24DD0703672E0741f00");
-console.log(balance);
+const main = async () => {
+  // get balance for address
+  const balance = await provider.getBalance(address);
+  console.log(balance);
+  console.log(
+    `\n ETH Balance of ${address} --> ${ethers.utils.formatEther(
+      balance)} ETH\N `
+  );
+};
+main();
