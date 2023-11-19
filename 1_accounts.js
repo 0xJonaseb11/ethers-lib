df9e83e9d9330851949571d5017790ae667f9e42
jonas-sebera <sebejaz99@gmail.com>
2023-11-19 12:59:26 +0200

    update

--- 1_accounts.js
+++ 1_accounts.js
@@ -1,1 +1,10 @@
 const { ethers }  = require("ethers");
+const INFURA_ID = "9d315598f135468a8184cc27a81a8646"
+
+// use Infura node 
+
+
+const provider = new ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/${INFURA_ID}`);
+
+
+// 49df6b4d7d7d4727a9e499a46bfb003d
