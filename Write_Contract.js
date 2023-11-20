d2c5b52363e0a0781190ddf33ed2f72607cc8431
jonas-sebera <sebejaz99@gmail.com>
2023-11-20 21:29:46 +0200

    write update

--- Write_Contract.js
+++ Write_Contract.js
@@ -12,7 +12,24 @@ const account2 = "";
 const privatekey1 = "";
 const wallet = new ethers.Wallet(privatekey1, provider);
 
+const ERC20_ABI = [
+    "function balance(address) view returns(uint)",;
+    "function transfer(address to, uint amount) returns(bool)",
+]
+const address = "Chainlink_token_address";
+const contract = new ethers.Contract(address, ERC20_ABI, provider);
+
 const main = async () => {
 
+    const balance = await contract.balanceOf(account1);
+    
+    console.log(`\n Reading from: ${address}`);
+    console.log(`Balance of sender: ${balance}`);
+
+    // connect to wallet
+    const contractWithWallet = contract.connect(wallet);
+    const tx = contractWithWallet.transfer(account1, balance);
+    // wait fot tx to be mined
+    await tx.wait();
 }
 main();
