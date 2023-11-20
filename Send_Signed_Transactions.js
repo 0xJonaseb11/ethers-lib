98d1fd98df53c4dd9e5bfb068c400d55899322a5
jonas-sebera <sebejaz99@gmail.com>
2023-11-20 21:06:49 +0200

    update accounts

--- Send_Signed_Transactions.js
+++ Send_Signed_Transactions.js
@@ -1,4 +1,4 @@
-const { ethers } = require("ethers");
+const { ethers, EtherSymbol } = require("ethers");
 
 const INFURA_ID ="";
 const provider = new ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/${INFURA_ID}`);
@@ -7,10 +7,25 @@ const account1 = "0x1f334285efdbe58034d3f24dd0703672e0741f00"; //sender
 const account2 = "etherscan_account_address"; // recipient
 
 const privatekey1 = "sender's_private_key";
+const wallet = new ethers.Wallet(privatekey1, provider);
+
 
 
 const main = async () => {
-    
+    // shoe account 1 balance before transfer
+    const senderBalanceBefore = await provider.getBalance(account1);
+    // show account 2 balance before transfer
+    const receiverBalanceBefore = await provider.getBalance(account2);
 
+    // fetch
+    console.log(`\nSender Balance Before: ${ethers.utils.formatEther(senderBalanceBefore)}`);
+    console.log(`\nReceiver Balance Before: ${receiverBalanceBefore}`);
+    
+    // sent ether
+    const tx = await wallet.sendTransaction({ to: account2, value: ethers.utils.parseEther("0.025")});
+    
+    // fetch tx after being mined
+    await tx.wait();
+    console.log(tx);
 }
 main();
