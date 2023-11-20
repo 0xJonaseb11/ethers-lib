6ee232784c1ef91345bcb561236bd17d2c6a9148
jonas-sebera <sebejaz99@gmail.com>
2023-11-20 21:53:51 +0200

    update inspect block

--- Inspecting_block.js
+++ Inspecting_block.js
@@ -1,4 +1,4 @@
-const { ethers }  = require("ethers");
+const { ethers, Transaction }  = require("ethers");
 const INFURA_ID = "";
 
 const provider = new ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/${INFURA_ID}`);
@@ -11,5 +11,12 @@ const main = async () => {
     const blockInfo = await provider.getBlock(block);
 
     console.log(`BlockInfo : ${blockInfo}`);
+
+    // getting overall txs
+    const { transactions } = await provider.getBlockWithTransactions(block);
+    
+    // log them to console
+    console.log(`'nLogging first transaction in block: \n`);
+    console.log(transactions[0]);
 }
 main();
