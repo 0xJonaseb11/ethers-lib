87e5fb2020be155821b14a3075c3f91fa193fde7
jonas-sebera <sebejaz99@gmail.com>
2023-11-20 20:54:45 +0200

    update signed

---
+++ Send_Signed_Transactions.js
@@ -0,0 +1,16 @@
+const { ethers } = require("ethers");
+
+const INFURA_ID ="";
+const provider = new ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/${INFURA_ID}`);
+
+const account1 = "0x1f334285efdbe58034d3f24dd0703672e0741f00"; //sender
+const account2 = "etherscan_account_address"; // recipient
+
+const privatekey1 = "sender's_private_key";
+
+
+const main = async () => {
+    
+
+}
+main();
