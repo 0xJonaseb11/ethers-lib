08ba1aad964b5f8a6ed9eaebefd56f97843a4dd9
Jaz-3-0 <sebejaz99@gmail.com>
2024-01-02 18:19:52 +0200

    init inspect blocks

--- Inspecting_block copy.js
+++ Inspecting_block copy.js
@@ -3,3 +3,19 @@ const INFURA_ID = '';
 
 const provider = new ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/${INFURA_ID}`);
 
+const main = async () => {
+    const block = await provider.getBlockNumber();
+
+    console.log(`\n BLock Number: ${block}`);
+
+    const blockInfo = await provider.getBlock(block);
+
+    console.log(`\n Block Info: ${blockInfo}`);
+
+    // getting overall txs
+    const { transactions } = await provider.getBlockWithTransactions(block);
+
+    // to the console
+    console.log(`\n Logging first transaction in block: \n`);
+    console.log(transactions([0]);
+}
